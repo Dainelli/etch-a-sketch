82f3f0db2bfc4f00341650ff5a65b0cf914e38a1
Dainelli <daniel.sf.freire@gmail.com>
2022-06-10 18:04:04 +0100

    grid

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Create a webpage with a 16x16 grid of square divs
 const container = document.getElementById("container");
+console.log("container, motherf*cker");
 
 makeRows(16, 16);
+console.log("call function, motherf*cker");
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell).className = "grid-item";
-    };
-};
-  
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
+}
 
 // put them in another div (container)
 
