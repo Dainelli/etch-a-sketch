547437f3d6d46cf233829205f148753e679d515c
Dainelli <daniel.sf.freire@gmail.com>
2022-06-10 17:00:12 +0100

    grid

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // Create a webpage with a 16x16 grid of square divs
+const container = document.getElementById("container");
+
+makeRows(16, 16);
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+  
 
 // put them in another div (container)
 
